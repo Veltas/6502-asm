67d5068b9ce2af91b2c6bc6a03f257e94f6d86e5
Christopher Leonard <chris.j.leonard@gmx.com>
2018-04-29 22:22:17 +0100

    improved/fixed untested example 6502

--- test.s
+++ test.s
@@ -2,8 +2,8 @@
 
 	.zero
 
-	; 8 16-bit pseudo-registers
-reg0
+; 8 16-bit pseudo-registers
+reg0:
 	.dsb 2
 reg1
 	.dsb 2
@@ -20,74 +20,90 @@ reg6
 reg7
 	.dsb 2
 
-	; 8-bit register reserved for use by pseudo-instructions
+; 8-bit register reserved for use by pseudo-instructions
 tmp
 	.dsb 1
 
-	; 16-bit Data Stack Pointer
+; 16-bit Data Stack Pointer
 dsp
 	.dsb 2
 
-	; 16-bit Data stack Base Pointer
+; 16-bit Data stack Base Pointer
 dbp
 	.dsb 2
 
+#define PROG_START $200
+#define DSP_START  $FF00
+
+; Startup code, wherever that needs to go
 	.text
-	* = 512
+	* = PROG_START
 startup
-	; transfer reg0 to reg1
-	lda #<reg0: jsr push_a
-	lda #<reg1: jsr push_a
+	; DSP_START -> dsp
+	ldx #0
+	lda #< DSP_START
+	sta dsp, x
+	lda #> DSP_START
+	inx
+	sta dsp, x
+
+	; transfer16_zp(reg0, reg1)
+	lda #< reg0
+	jsr push_a
+	lda #< reg1
+	jsr push_a
 	jsr transfer16_zero
 	jsr inc2_dsp
+
+	; don't exit
+	jmp *
 
 ; pushes A onto data stack
 	.text
 push_a
-	.(
+.(
+	; store a
 	pha
-	lda #$FF
-	dec dsp
-	cmp dsp
-	bne no_carry
+	; dsp-1 -> dsp
+	lda dsp
+	beq carry
 		dec dsp+1
-no_carry
-	ldx #0
+carry
+	dec dsp
+	; retrieve a
 	pla
-	sta (dsp,x)
+	; a -> (dsp)
+	ldy #0
+	sta (dsp), y
 	rts
-	.)
+.)
 
 ; pulls A from data stack
 	.text
 pull_a
-	.(
+.(
 	ldx #0
 	lda (dsp,x)
 	inc dsp
-	bne no_carry
+	beq carry
+		rts
+carry
 		inc dsp+1
-no_carry
-	rts
-	.)
+		rts
+.)
 
 ; pushes reg0 to data stack
 	.text
 push_reg0
-	.(
-	lda #$FF
-	dec dsp
-	cmp dsp
-	bne no_carry1
-		dec dsp+1
-		dec dsp
-		bne no_carry2
-no_carry1
-		dec dsp
-		cmp dsp
-		bne no_carry2
-			dec dsp+1
-no_carry2
+	; dsp-2 -> dsp
+	clc
+	lda dsp
+	adc #2
+	sta dsp
+	lda dsp+1
+	adc #0
+	sta dsp+1
+	; reg0 -> (dsp)
 	ldy #0
 	lda reg0
 	sta (dsp),y
@@ -95,104 +111,89 @@ no_carry2
 	iny
 	sta (dsp),y
 	rts
-	.)
 
 ; pulls reg0 from data stack
 	.text
 pull_reg0
-	.(
+	; (dsp) -> reg0
 	ldy #0
 	lda (dsp),y
 	sta reg0
 	iny
 	lda (dsp),y
 	sta reg0+1
-	inc dsp
-	bne no_carry1
-		inc dsp+1
-		inc dsp
-		rts
-no_carry1
-		inc dsp
-		bne no_carry2
-			inc dsp+1
-no_carry2
+	; dsp+2 -> dsp
+	clc
+	lda dsp
+	adc #2
+	sta dsp
+	lda dsp+1
+	adc #0
+	sta dsp+1
 	rts
-	.)
 
 ; decrement data stack pointer
 	.text
 dec_dsp
-	.(
-	dec dsp
-	lda #$FF
-	cmp dsp
-	bne no_carry
+.(
+	lda dsp
+	beq carry
+		dec dsp
+		rts
+carry
+		dec dsp
 		dec dsp+1
-no_carry
-	rts
-	.)
+		rts
+.)
 
 ; decrement data stack pointer by two
 	.text
 dec2_dsp
-	.(
-	dec dsp
-	lda #$FF
-	cmp dsp
-	bne no_carry
-		dec dsp+1
-		dec dsp
-		rts
-no_carry1
-		dec dsp
-		cmp dsp
-		bne no_carry
-			dec dsp+1
-no_carry2
+	clc
+	lda dsp
+	sbc #2
+	sta dsp
+	lda dsp+1
+	sbc #0
+	sta dsp+1
 	rts
-	.)
 
 ; increment data stack pointer
 	.text
 inc_dsp
-	.(
+.(
 	inc dsp
-	bne no_carry
+	beq carry
+		rts
+carry
 		inc dsp+1
-no_carry
-	rts
-	.)
+		rts
+.)
 
 ; increment data stack pointer by two
 	.text
 inc2_dsp
-	.(
-	inc dsp
-	bne no_carry1
-		inc dsp+1
-		inc dsp
-		rts
-no_carry1
-		inc dsp
-		bne no_carry2
-			inc dsp+1
-no_carry2
+	clc
+	lda dsp
+	adc #2
+	sta dsp
+	lda dsp+1
+	adc #0
+	sta dsp+1
 	rts
-	.)
 
 ; transfers 16-bit contents of zero page $1 to zero page $2
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; void transfer16_zero(Byte r1, Byte r2) ;
+; void transfer16_zp(Byte r1, Byte r2) ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	.text
-transfer16_zero
-	ldx #0
-	ldy dsp,x   ; load source address
-	lda $0, y   ; load first byte
-	inx
-	sta (dsp,x) ; store first result value
-	lda $0+1, y ; load second byte
-	ldy dsp,x   ; load dest address
-	sta $0+1,y  ; store second result value
+transfer16_zp
+	ldy #0
+	lda (dsp), y ; load source ZP address
+	tax
+	iny
+	lda (dsp), y ; load dest ZP address
+	tay
+	lda #0, x
+	sta #0, y
 	rts
